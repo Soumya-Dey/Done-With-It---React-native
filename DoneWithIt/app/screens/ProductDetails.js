71e7f25cbe0ed9312d2a9096c5e5306ae3dae622
Soumya Dey <soumyadey200@hotmail.com>
2020-11-22 21:18:53 +0530

    product details screen started

--- DoneWithIt/app/screens/ProductDetails.js
+++ DoneWithIt/app/screens/ProductDetails.js
@@ -5,6 +5,8 @@ import {
   Text,
   View,
   TouchableOpacity,
+  Image,
+  ScrollView,
 } from "react-native";
 import { connect } from "react-redux";
 import Constants from "expo-constants";
@@ -14,17 +16,29 @@ import { CommonActions } from "@react-navigation/native";
 import Splash from "./Splash";
 
 const ProductDetails = ({ product, productLoading, navigation }) => {
+  console.log(product);
+
   return !productLoading && product ? (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <TouchableOpacity
         style={styles.backButton}
         activeOpacity={0.6}
-        onPress={() => navigation.dispatch(CommonActions.goBack())}
+        onPress={() => navigation.pop()}
       >
         <Ionicons name="ios-arrow-back" size={24} color="black" />
       </TouchableOpacity>
-      <Text>{product.title}</Text>
-    </SafeAreaView>
+      <ScrollView>
+        <Image
+          source={{ uri: product.imageUrls[0] }}
+          style={styles.productImage}
+        ></Image>
+        <View style={styles.infoContainer}>
+          <Text style={styles.titleText}>{product.title}</Text>
+          <Text style={styles.priceText}>${product.priceInDollar}</Text>
+          <Text style={styles.descText}>{product.description}</Text>
+        </View>
+      </ScrollView>
+    </View>
   ) : (
     <Splash />
   );
@@ -36,13 +50,14 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
     // alignItems: "center",
     // padding: 18,
-    marginTop: Constants.statusBarHeight + 18,
+    // marginTop: Constants.statusBarHeight + 18,
   },
   backButton: {
     position: "absolute",
+    top: Constants.statusBarHeight + 18,
     left: 18,
-    height: 44,
-    width: 44,
+    height: 42,
+    width: 42,
     borderRadius: 22,
     backgroundColor: "#ffffff",
     justifyContent: "center",
@@ -55,6 +70,30 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
+  },
+  productImage: {
+    width: "100%",
+    height: 300,
+  },
+  infoContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 18,
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginBottom: 8,
+    color: "#404040",
+  },
+  priceText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#1dd1a1",
+    marginBottom: 16,
+  },
+  descText: {
+    fontSize: 14,
+    color: "gray",
   },
 });
 
